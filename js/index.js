860e0ccb9455421c6b5cd47d2c639523b1ed0348
ifylala <ifeanyichukwunwabuokei@gmail,com>
2019-01-31 20:51:42 +0100

    js file

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function showImage() {
+    var img = document.getElementById('myImg');
+    var img2 = document.getElementById('Img2')
+    img.style.visibility = 'hidden';
+    img2.style.visibility = 'visible';
+}
+
+{/* <a href="javascript:setImageVisible('myImageId', true)">show image</a>
+<a href="javascript:setImageVisible('myImageId', false)">hide image</a> */}
